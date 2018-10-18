b42bc75ba63b53eb676fe7ddff6485750944edb6
Kevin-CodeCrew <kevin@code-crew.org>
2018-10-18 07:56:06 -0500

    Initial Commit

---
+++ ProTwo/appTwo/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from appTwo.models import User
+
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'apptwo/index.html')
+
+
+def users(request):
+    # THIS AINT WORKING
+    return render(request, 'apptwo/users.html')
